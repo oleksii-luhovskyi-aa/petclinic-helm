d3a5871f4fe4f4c7da70073f78e479b64ce1e9e8
Oleksii Luhovskyi <oleksii.luhovskyi.aa@gmail.com>
2024-12-10 23:18:25 +0200

    Changed tag search command

--- execute-helm-installation.sh
+++ execute-helm-installation.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-IMAGE_TAG=$(gcloud artifacts docker images list us-central1-docker.pkg.dev/atlantean-yeti-440716-a1/jenkins-docker-repo --filter="package=us-central1-docker.pkg.dev/atlantean-yeti-440716-a1/jenkins-docker-repo/petclinic" --sort-by="~UPDATE_TIME" --limit=1 --format="value(format("{0}",version))")
+IMAGE_TAG=$(gcloud artifacts docker images list us-central1-docker.pkg.dev/atlantean-yeti-440716-a1/jenkins-docker-repo/petclinic --sort-by=~update_time --include-tags --limit=1 --format="value(tags)")
 
 helm uninstall petclinic-deploy
 
