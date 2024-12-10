1396cf4c5a1db4ad3677bfde2536f88e583fe0e7
Oleksii Luhovskyi <oleksii.luhovskyi.aa@gmail.com>
2024-12-10 22:06:34 +0200

    Added executable file for helm run (small fix2)

--- execute-helm-installation.sh
+++ execute-helm-installation.sh
@@ -2,6 +2,6 @@
 
 IMAGE_TAG=$(gcloud artifacts docker images list us-central1-docker.pkg.dev/atlantean-yeti-440716-a1/jenkins-docker-repo --filter="package=us-central1-docker.pkg.dev/atlantean-yeti-440716-a1/jenkins-docker-repo/petclinic" --sort-by="~UPDATE_TIME" --limit=1 --format="value(format("{0}",version))")
 
-sh "helm uninstall petclinic-deploy"
+helm uninstall petclinic-deploy
 
-sh "helm install --set image.tag=${IMAGE_TAG} petclinic-deploy petclinic-simple-chart/"
+helm install --set image.tag=${IMAGE_TAG} petclinic-deploy petclinic-simple-chart/
