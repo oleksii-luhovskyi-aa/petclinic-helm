d00f367fb4bac8af726978266dff5cd34330758d
Oleksii Luhovskyi <oleksii.luhovskyi.aa@gmail.com>
2024-12-22 22:50:44 +0200

    Helm execute fix

--- execute-helm-installation.sh
+++ execute-helm-installation.sh
@@ -8,6 +8,6 @@ NEED_RECREATE=$3
 
 IMAGE_TAG=$(gcloud artifacts docker images list us-central1-docker.pkg.dev/atlantean-yeti-440716-a1/jenkins-docker-repo/petclinic --sort-by=~update_time --include-tags --limit=1 --format="value(tags)")
 
-helm uninstall deploy-${SERVICE_NAME}
+helm uninstall deployment-${SERVICE_NAME}
 
 helm install --set image.tag=${IMAGE_TAG} --set service.name=${SERVICE_NAME} --set service.port=${SERVICE_PORT} --set needRecreate=${NEED_RECREATE} deployment-${SERVICE_NAME} petclinic-simple-chart/
