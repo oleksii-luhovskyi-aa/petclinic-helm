db507b3e55782b0f09e0af2c43cba301d9b499c4
Oleksii Luhovskyi <oleksii.luhovskyi.aa@gmail.com>
2024-12-22 20:16:35 +0200

    Changed execution starter

--- execute-helm-installation.sh
+++ execute-helm-installation.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+SERVICE_NAME=$1
+
+SERVICE_PORT=$2
+
 IMAGE_TAG=$(gcloud artifacts docker images list us-central1-docker.pkg.dev/atlantean-yeti-440716-a1/jenkins-docker-repo/petclinic --sort-by=~update_time --include-tags --limit=1 --format="value(tags)")
 
 helm uninstall petclinic-deploy
 
-helm install --set image.tag=${IMAGE_TAG} petclinic-deploy petclinic-simple-chart/
+helm install --set image.tag=${IMAGE_TAG} --set service.name=${SERVICE_NAME} --set service.port=${SERVICE_PORT} deployment-${SERVICE_NAME} petclinic-simple-chart/
